f4f70579517c27771885ab526c8d27285de9a523
liangsining <95091058+liangsining@users.noreply.github.com>
2022-03-03 01:15:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,35 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+from pyngrok import ngrok
+
+%%writefile app.py
 import streamlit as st
+import tensorflow as tf
 
-"""
-# Welcome to Streamlit!
+import numpy as np
+from PIL import Image # Strreamlit works with PIL library very easily for Images
+import cv2
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+model_path='/content/drive/MyDrive/MLH Project/model-recent.h5'
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("COVID-19 Identification Using CT Scan")
+upload = st.file_uploader('Upload a CT scan image')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if upload is not None:
+  file_bytes = np.asarray(bytearray(upload.read()), dtype=np.uint8)
+  opencv_image = cv2.imdecode(file_bytes, 1)
+  opencv_image = cv2.cvtColor(opencv_image,cv2.COLOR_BGR2RGB) # Color from BGR to RGB
+  img = Image.open(upload)
+  st.image(img,caption='Uploaded Image',width=300)
+  if(st.button('Predict')):
+    model = tf.keras.models.load_model(model_path)
+    x = cv2.resize(opencv_image,(100,100))
+    x = np.expand_dims(x,axis=0)    
+    y = model.predict(x)
+    ans=np.argmax(y,axis=1)
+    if(ans==0):
+      st.title('COVID')
+    elif(ans==1):
+      st.title('Healthy')
+    else:
+      st.title('Other Pulmonary Disorder')
+    
+    
